52ab9f60eedd0920f8a0bb2ae34dad67bc9c85a2
Denis <zabavnikov.com@gmail.com>
2021-11-15 11:34:20 +0700

    update

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 /* eslint-disable import/prefer-default-export */
-import { default as VTipTap } from './editor/Component.vue';
-import { default as VTipTapOutput } from './output/Component.vue';
+import VTipTap from './editor/Component.vue';
+import VTipTapOutput from './output/Component.vue';
+
+export default {
+  VTipTap,
+  VTipTapOutput
+};
