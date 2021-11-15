2f6dfc97f1c8050ca581d0aab36cb20eb671a77e
Denis <zabavnikov.com@gmail.com>
2021-11-15 13:25:57 +0700

    update

--- src/components/editor/Component.vue
+++ src/components/editor/Component.vue
@@ -64,7 +64,7 @@ export default {
 
   mounted() {
     this.editor = new Editor({
-      content: this.value,
+      content: JSON.parse(this.value),
       extensions: [
         StarterKit,
         Placeholder.configure({
