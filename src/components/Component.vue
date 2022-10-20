284c2d972134e4f358757a7f8ab4510755e17ab5
Denis <zabavnikov.com@gmail.com>
2022-10-20 14:46:18 +0700

    update

--- src/components/Component.vue
+++ src/components/Component.vue
@@ -23,7 +23,7 @@
       </button>
     </floating-menu>
     <editor-content :editor="editor" @cover="$emit('update:coverId', $event)" />
-    <input type="file" ref="images" style="display: none;" @change="addImage" multiple>
+    <input type="file" ref="images" style="display: none;" @change="addImage" multiple accept="image/*">
   </div>
 </template>
 <script>
